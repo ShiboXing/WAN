242bc8110332cf05be223fa42652a20f153ee3ee
Shibo Xing <shx26@pitt.edu>
2021-11-10 17:08:46 -0500

    feat: add proj4, annotate paper

--- shx26/stat_display.h
+++
@@ -1,3 +0,0 @@
-void print_stat(bool isRcv, double duration, long long unsigned int max_seq,
-        long long unsigned int cum_seq, long long unsigned int re_pkts, long long unsigned int lost_pkts,
-            double avg_delay, double min_delay, double max_delay);
