d061fea3059b11c6409f9dd9b17a18cbd344c35b
qil77 <qil77@sender.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-11-29 17:54:23 -0600

    feat: add to setup.sh

--- proj4/set_up.sh
+++ proj4/set_up.sh
@@ -1,4 +1,12 @@
+# add BBR protocol to /etc/sysctl.conf:
+# 	net.core.default_qdisc=fq
+#	net.ipv4.tcp_congestion_control=bbr
+
+# aliasing programs
+alias iperf3='~/WAN/proj4/iperf-3.10.1/src/iperf3'
+
 # Step 1: set bandwidth to 10mbps (during GENI node setup)
 # Step 2: set RTT to 40ms
+sudo tc qdisc add dev eth1 root netem delay 40ms
 sudo tc qdisc replace dev eth1 root netem delay 40ms
-alias iperf3='~/WAN/proj4/iperf-3.10.1/src/iperf3'
+
