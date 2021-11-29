7e8aaa401f65c9b690182aa15d07a9d9d1780cba
qil77 <qil77@sender.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-11-29 17:11:11 -0600

    feat: aliasing iperf'

--- proj4/set_up.sh
+++ proj4/set_up.sh
@@ -1,3 +1,4 @@
 # Step 1: set bandwidth to 10mbps (during GENI node setup)
 # Step 2: set RTT to 40ms
-sudo tc qdisc add dev eth1 root netem delay 40ms
+sudo tc qdisc replace dev eth1 root netem delay 40ms
+alias iperf3='~/WAN/proj4/iperf-3.10.1/src/iperf3'
