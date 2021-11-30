df9af7823bb1537777e88333fb98a78eacb49626
qil77 <qil77@router.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-11-30 15:57:47 -0600

    feat: add queue var check to exp1

--- proj4/exp1.sh
+++ proj4/exp1.sh
@@ -1,13 +1,18 @@
 
-HOST=`hostname`
-echo "hostname: $HOST"
-BDP=$(( $1 * 400 ))
-echo "BDP: $BDP"
+if [ -z "$1" ] 
+then
+	echo "please enter a BDP number for the bottleneck queue"
+else
+	HOST=`hostname`
+	echo "hostname: $HOST"
 
-if [[ "$HOST" == *"router"* ]]; then
-	echo 'setting router queue to $BDP BDPs'
-	sudo tc qdisc add dev eth2 root tbf rte 100mbit limit $BDPkb burst 10kb
-	sudo tc qdisc replace dev eth2 root tbf rate 100mbit limit $BDPkb burst 10kb
+	BDP=$(( $1 * 200 ))
+	echo "queue size: ${BDP}kb"
+
+	if [[ "$HOST" == *"router"* ]]; then
+		echo 'setting router queue to '$1' BDPs'
+		sudo tc qdisc add dev eth2 root tbf rate 100mbit limit ${BDP}kb burst 10kb
+		sudo tc qdisc replace dev eth2 root tbf rate 100mbit limit ${BDP}kb burst 10kb
+	fi
 fi
-
 
