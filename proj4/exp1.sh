3aa79cab4b868573d8ac6436b23eaf9b171252f5
qil77 <qil77@sender.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-11-30 15:21:33 -0600

    feat: add finite queue

--- proj4/exp1.sh
+++ proj4/exp1.sh
@@ -1,1 +1,13 @@
 
+HOST=`hostname`
+echo "hostname: $HOST"
+BDP=$(( $1 * 400 ))
+echo "BDP: $BDP"
+
+if [[ "$HOST" == *"router"* ]]; then
+	echo 'setting router queue to $BDP BDPs'
+	sudo tc qdisc add dev eth2 root tbf rte 100mbit limit $BDPkb burst 10kb
+	sudo tc qdisc replace dev eth2 root tbf rate 100mbit limit $BDPkb burst 10kb
+fi
+
+
