c0f50d5a9431f75f3577151bf0321ac8fe15a309
qil77 <qil77@sender.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-11-30 15:03:02 -0600

    feat: check hostname for RTT

--- proj4/set_up.sh
+++ proj4/set_up.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # add BBR protocol to /etc/sysctl.conf:
 # 	net.core.default_qdisc=fq
 #	net.ipv4.tcp_congestion_control=bbr
@@ -7,6 +9,11 @@ alias iperf3='~/WAN/proj4/iperf-3.10.1/src/iperf3'
 
 # Step 1: set bandwidth to 10mbps (during GENI node setup)
 # Step 2: set RTT to 40ms
-sudo tc qdisc add dev eth1 root netem delay 40ms
-sudo tc qdisc replace dev eth1 root netem delay 40ms
+HOST=`hostname`
+echo "hostname: $HOST"
+if [[ "$HOST" == *"sender"* ]]; then
+	echo 'setting RTT delay on sender'
+	sudo tc qdisc add dev eth1 root netem delay 40ms
+	sudo tc qdisc replace dev eth1 root netem delay 40ms
+fi
 
