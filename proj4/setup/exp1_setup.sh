16537a27392a47c7562ecfdbae9bcefa65494e1e
qil77 <qil77@sender.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-12-02 17:22:14 -0600

    feat: change to bfifi

--- proj4/setup/exp1_setup.sh
+++ proj4/setup/exp1_setup.sh
@@ -6,8 +6,7 @@ source testbed_setup.sh
 # Step 2: set bottleneck queue on router
 if [[ "$HOST" == *"router"* ]]; 
 	then
-    queue=$(( $1 * 200 ))
-    echo "setting router queue to '$1' BDPs, ${queue}kb"
-    sudo tc qdisc add dev eth2 root tbf rate 100mbit limit ${queue}kb burst 100kb
-    sudo tc qdisc replace dev eth2 root tbf rate 100mbit limit ${queue}kb burst 100kb
+    queue=$(( $1 * 200000 / 8 ))
+    echo "setting router queue to '$1' BDPs, ${queue} bytes"
+    sudo tc qdisc replace dev eth2 root bfifo limit ${queue}
 fi
