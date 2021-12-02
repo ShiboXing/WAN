d738b670afd6503da9cf9a34ade9eebcf658d490
qil77 <qil77@router.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-12-02 17:27:06 -0600

    fix: queue size

--- proj4/setup/exp1_setup.sh
+++ proj4/setup/exp1_setup.sh
@@ -6,7 +6,7 @@ source testbed_setup.sh
 # Step 2: set bottleneck queue on router
 if [[ "$HOST" == *"router"* ]]; 
 	then
-    queue=$(( $1 * 200000 / 8 ))
+    queue=$(( $1 * 400000 / 8 ))
     echo "setting router queue to '$1' BDPs, ${queue} bytes"
     sudo tc qdisc replace dev eth2 root bfifo limit ${queue}
 fi
