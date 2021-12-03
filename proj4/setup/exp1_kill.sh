2a1548393215a00cab1ce3eb26c780d856788a1f
qil77 <qil77@router.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-12-02 18:47:28 -0600

    feat: use fq, add reset code

--- proj4/setup/exp1_kill.sh
+++ proj4/setup/exp1_kill.sh
@@ -1,4 +1,6 @@
 
 lsof -i:$bbr_port -t | xargs kill -9
 lsof -i:$loss_port -t | xargs kill -9
-
+sudo tc qdisc del dev eth0 root
+sudo tc qdisc del dev eth1 root
+sudo tc qdisc del dev eth2 root
--- proj4/setup/exp1_setup.sh
+++ proj4/setup/exp1_setup.sh
@@ -6,7 +6,9 @@ source testbed_setup.sh
 # Step 2: set bottleneck queue on router
 if [[ "$HOST" == *"router"* ]]; 
 	then
-    queue=$(( $1 * 400000 / 8 ))
-    echo "setting router queue to '$1' BDPs, ${queue} bytes"
-    sudo tc qdisc replace dev eth2 root bfifo limit ${queue}
+    queue=$(( $1 * 16 ))
+    echo "setting router queue to '$1' BDPs, ${queue} packets"
+    sudo tc qdisc replace dev eth0 root fq limit ${queue}
+
+
 fi
