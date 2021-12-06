23c3d5043fffbc0d43d9f9cb3306f859dabf8fa7
qil77 <qil77@router.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-12-06 14:22:20 -0600

    fix: rtt in router

--- proj4/setup/exp1_setup.sh
+++ proj4/setup/exp1_setup.sh
@@ -7,15 +7,17 @@ source testbed_setup.sh
 if [[ "$HOST" == *"router"* ]]; 
 	then
     queue=$(( $2 * 1000000 / 1000 * $3 * $1 / 1000 ))
+    ott=$(( $3 / 2))
     echo "setting router queue to '$1' BDPs, ${queue}kb"
-    sudo tc qdisc replace dev eth2 root tbf rate 9mbit limit ${queue}kb burst 10kb
+    sudo tc qdisc replace dev eth2 root tbf rate 6mbit limit ${queue}kb burst 10kb
+    echo "setting router to sender single-trip time to ${ott}ms"
+    sudo tc qdisc replace dev eth1 root netem delay ${ott}ms
 fi
 
 # Step 2: set RTT to 40ms on sender
 if [[ "$HOST" == *"sender"* ]]; 
 	then
-	echo "setting eth1 RTT delay on sender to ${2}ms"
-	sudo tc qdisc add dev eth1 root netem delay $2ms
+	echo "setting eth1 one-trip delay on sender to ${2}ms"
 	sudo tc qdisc replace dev eth1 root netem delay $2ms
 fi
 
