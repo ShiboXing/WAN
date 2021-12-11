edd5328ed125b4a19ff5ae28640aa4c13bfc16fe
qil77 <qil77@router.qyl-project1.ch-geni-net.geni.uchicago.edu>
2021-12-10 21:36:32 -0600

    fix: help message

--- proj4/setup/exp1_setup.sh
+++ proj4/setup/exp1_setup.sh
@@ -21,20 +21,22 @@ elif [ -z "$4" ]
 elif [ -z "$5" ] 
     then
     echo $exp1_setup_help
+else
+	rate=$(($2 * 1000))
+	queue=$(( $2 * 1000000 / 1000 * $3/2 * $1 / 1000 / $4))
+
+	ott=$3
+	# Step 2: set bottleneck queue on router
+	if [[ "$HOST" == *"router"* ]]; 
+		then
+	    echo "setting router queue to '$1' BDPs, ${queue}kb"
+	    echo "setting bandwidth limit to $2mbps, ${rate}kbps"
+	    sudo tc qdisc replace dev eth2 root tbf rate ${rate}kbit limit ${queue}kb burst 10kb
+	    echo "setting router to sender single-trip time to ${ott}ms"
+	    echo "setting router loss rate to ${5} percent"
+	    sudo tc qdisc replace dev eth1 root netem delay ${ott}ms loss $5
+	fi
 fi
 
 
-rate=$(($2 * 1000))
-queue=$(( $2 * 1000000 / 1000 * $3/2 * $1 / 1000 / $4))
-
-ott=$3
-# Step 2: set bottleneck queue on router
-if [[ "$HOST" == *"router"* ]]; 
-	then
-    echo "setting router queue to '$1' BDPs, ${queue}kb"
-    echo "setting bandwidth limit to $2mbps, ${rate}kbps"
-    sudo tc qdisc replace dev eth2 root tbf rate ${rate}kbit limit ${queue}kb burst 10kb
-    echo "setting router to sender single-trip time to ${ott}ms"
-    sudo tc qdisc replace dev eth1 root netem delay ${ott}ms loss $5
-fi
 
